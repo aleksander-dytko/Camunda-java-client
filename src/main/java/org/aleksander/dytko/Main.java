7be963dae22f562c1bd48e764449f94371f1bd03
Aleksander Dytko <102789122+aleksander-dytko@users.noreply.github.com>
2025-08-21 15:40:42 +0200

    Add Camunda client integration and example process deployment

--- src/main/java/org/aleksander/dytko/Main.java
+++ src/main/java/org/aleksander/dytko/Main.java
@@ -1,17 +1,103 @@
 package org.aleksander.dytko;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import io.camunda.client.CamundaClient;
+import io.camunda.client.CredentialsProvider;
+import io.camunda.client.api.response.ActivatedJob;
+import io.camunda.client.api.response.DeploymentEvent;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.client.api.response.Topology;
+import io.camunda.client.api.worker.JobClient;
+import io.camunda.client.api.worker.JobHandler;
+import io.camunda.client.api.worker.JobWorker;
+import io.camunda.client.impl.NoopCredentialsProvider;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Main class to connect to Camunda 8 Gateway using gRPC and REST.
+ * Replace the grpcAddress and restAddress with your actual addresses.
+ */
+public class Main {
+
+    private static final String grpcAddress = "http://localhost:26500";
+    private static final String restAddress = "http://localhost:8080/v2";
+
+    public static void main(String[] args) {
+        // For local development (no authentication)
+        CredentialsProvider credentialsProvider = new NoopCredentialsProvider();
+
+        try (CamundaClient client = CamundaClient.newClientBuilder()
+                .grpcAddress(URI.create(grpcAddress)).usePlaintext()
+                .restAddress(URI.create(restAddress))
+                .credentialsProvider(credentialsProvider)
+                .build()) {
+
+            // Test the connection
+            Topology topology= client.newTopologyRequest().send().join();
+            System.out.println(topology);
+
+            // Deploy a process definition
+            System.out.println("Deploying process definition...");
+            final DeploymentEvent deploymentEvent = client.newDeployResourceCommand()
+                    .addResourceFromClasspath("exampleProcess.bpmn")
+                    .send()
+                    .join();
+
+            System.out.println("Deployment successful: " + deploymentEvent.getKey());
+
+            // Create a process instance
+            System.out.println("Creating process instance...");
+            final ProcessInstanceEvent processInstanceEvent = client.newCreateInstanceCommand()
+                    .bpmnProcessId("exampleProcess")
+                    .latestVersion()
+                    .variables(Map.of("orderId", "12345"))
+                    .send()
+                    .join();
+
+            System.out.println("Process instance created: " + processInstanceEvent.getProcessInstanceKey());
+
+            // Register a job worker to handle jobs of type "send-email"
+            System.out.println("Registering job worker...");
+
+            // The job type must match the one defined in the BPMN process
+            final String jobType = "send-email";
+
+            try (final JobWorker workerRegistration = client.newWorker()
+                    .jobType(jobType)
+                    .handler(new ExampleJobHandler())
+                    .timeout(Duration.ofSeconds(10))
+                    .open()) {
+
+                System.out.println("Job worker opened and receiving jobs.");
+
+                // run until System.in receives exit command
+                waitUntilSystemInput("exit");
+            }
+
+
+            }
+        }
+
+    private static void waitUntilSystemInput(final String exitCode) {
+        try (final Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                final String nextLine = scanner.nextLine();
+                if (nextLine.contains(exitCode)) {
+                    return;
+                }
+            }
+        }
+    }
+
+    private static class ExampleJobHandler implements JobHandler {
+        @Override
+        public void handle(final JobClient client, final ActivatedJob job) {
+            // here: business logic that is executed with every job
+            System.out.println(job);
+            client.newCompleteCommand(job.getKey()).send().join();
         }
     }
 }
